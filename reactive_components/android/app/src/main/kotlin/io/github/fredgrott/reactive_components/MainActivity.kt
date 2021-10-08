a5b01bfea3a006a0cb99f2e38a5e2f82cb935b79
Fred Grott <fred.grott@gmail.com>
2021-10-08 18:39:18 -0500

    reactive components wip
    reactive components wip

---
+++ reactive_components/android/app/src/main/kotlin/io/github/fredgrott/reactive_components/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.fredgrott.reactive_components
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tiny_state_solution/android/app/src/main/kotlin/io/github/fredgrott/tiny_state_solution/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.fredgrott.tiny_state_solution
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
